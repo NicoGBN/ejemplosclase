75d5c8e22ca79cfa8d79846134f1123fc815fd3e
Adrian Meca <adrian.meca@globant.com>
2014-03-28 17:49:34 -0300

    adding gitignore

--- workspace2014/EstructuraFor/src/ejemploFor/EjemploFor.java
+++ workspace2014/EstructuraFor/src/ejemploFor/EjemploFor.java
@@ -1,8 +1,10 @@
 package ejemploFor;
 
+import java.io.IOException;
+
 public class EjemploFor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int i=0;
 		System.out.println("while");
 		while (i<=5){
@@ -16,11 +18,11 @@ public class EjemploFor {
 			
 			System.out.println(i);
 			i--;
-		} while(i>=5);
-		
+		} while(i>=5);		
 		for(int j=0;j<5;j++){
 			
 			System.out.println("J="+j);
+			
 		}
 
 	}
